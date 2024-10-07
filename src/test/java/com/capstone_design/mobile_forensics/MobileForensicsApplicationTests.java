c250aee50a5ebe2cc41a3c51ba45e3457b167278
3un0ia <kj122kj@naver.com>
2024-10-07 22:34:57 +0900

    build : project initial settings

---
+++ src/test/java/com/capstone_design/mobile_forensics/MobileForensicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone_design.mobile_forensics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileForensicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
