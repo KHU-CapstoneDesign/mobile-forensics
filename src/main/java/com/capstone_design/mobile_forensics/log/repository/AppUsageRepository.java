3b2e26c24d686bead714da655f8d28b6911da348
3un0ia <kj122kj@naver.com>
2024-11-19 22:21:42 +0900

    feat : 사용자 데이터 기반 전체 데이터 검색 및 반환 로직 구현

--- src/main/java/com/capstone_design/mobile_forensics/log/repository/AppUsageRepository.java
+++ src/main/java/com/capstone_design/mobile_forensics/log/repository/AppUsageRepository.java
@@ -2,11 +2,18 @@ package com.capstone_design.mobile_forensics.log.repository;
 
 import com.capstone_design.mobile_forensics.log.entity.AppUsageLog;
 import com.capstone_design.mobile_forensics.log.entity.GPSMetadata;
+import com.capstone_design.mobile_forensics.log.entity.TakenPictureLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-public interface AppUsageRepository extends LogRepository {
-    List<AppUsageLog> findAllByTimestamp(LocalDateTime timestamp);
+@Repository
+public interface AppUsageRepository extends JpaRepository<AppUsageLog, Long> {
+    @Query(value = "SELECT * FROM app_usage_log u WHERE " +
+            "ABS(TIMESTAMPDIFF(MINUTE, u.timestamp, :timestamp)) <= 30",
+            nativeQuery = true)
+    List<AppUsageLog> findAllWithin30Minutes(@Param("timestamp") LocalDateTime timestamp);
 }
--- src/main/java/com/capstone_design/mobile_forensics/web/UserRepository.java
+++ src/main/java/com/capstone_design/mobile_forensics/web/UserRepository.java
@@ -1,10 +1,11 @@
 package com.capstone_design.mobile_forensics.web;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<UserData, Long> {
     @Override
     UserData save(UserData entity);
