e7cb8c7eb17a21e40d3b90da23b17298f3e7c418
3un0ia <kj122kj@naver.com>
2024-11-22 21:17:37 +0900

    chore : 파일 서비스 인터페이스 추가 및 구현체 수정

--- src/main/java/com/capstone_design/mobile_forensics/file/FileController.java
+++ src/main/java/com/capstone_design/mobile_forensics/file/FileController.java
@@ -1,17 +1,12 @@
 package com.capstone_design.mobile_forensics.file;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api")
--- src/main/java/com/capstone_design/mobile_forensics/file/FileService.java
+++ src/main/java/com/capstone_design/mobile_forensics/file/FileService.java
@@ -1,125 +1,21 @@
 package com.capstone_design.mobile_forensics.file;
 
-import com.capstone_design.mobile_forensics.log.LogProcessService;
-import com.capstone_design.mobile_forensics.log.LogProcessServiceImpl;
-import com.capstone_design.mobile_forensics.web.WebService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
+import com.capstone_design.mobile_forensics.file.api.SafeSearchResponse;
+import com.capstone_design.mobile_forensics.web.UserData;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
 
-@Service
-@Slf4j
-public class FileService {
-
-    @Autowired
-    private WebService webService;
-    @Autowired
-    private LogProcessService logService;
-    @Autowired
-    private ImageFileRepository imageRepository;
-
-    public ResponseEntity signal(String signal){
-
-        // 파일 전송 시작과 끝 -> 프론트엔드에 알림(POST)
-        if (signal.equalsIgnoreCase("start"))  {
-            log.info("Processing Started.");
-            ResponseEntity<String> response = webService.sendNotificationStart();
-            return response;
-        } else if (signal.equalsIgnoreCase("end")) {
-            log.info("Processing Completed");
-            ResponseEntity<String> response = webService.sendNotificationEnd();
-            return response;
-        }
-        return null;
-    }
-
-    public void fileUpload(MultipartFile file, String parentDir) throws IOException {
-        String fileName = file.getOriginalFilename();
-        log.info("File received.\tFile Name = {}", fileName);
-
-        // 파일 확장자 확인
-        // -> 이미지 파일이면 데이터베이스 저장
-        // -> 로그 파일이면 로그 파싱 후 저장
-        switch (getFileExtension(fileName).toLowerCase()) {
-            case "txt" :
-                ResponseEntity response = logService.parseLogs(file);// 로그파일 처리
-                break;
-            case "jpg" :
-            case "jpeg" :
-            case "png" :
-                processImageFile(file, parentDir); // 이미지파일 처리
-                break;
-            default :
-                throw new IllegalArgumentException("Unsupported File Type: " + fileName);
-        }
-    }
-
-    // 파일 확장자 추출
-    private String getFileExtension(String fileName) {
-        int lastIndex = fileName.lastIndexOf('.');
-        if (lastIndex == -1) {
-            throw new IllegalArgumentException("No file extension found for file: " + fileName);
-        }
-        return fileName.substring(lastIndex + 1);
-    }
-
-    // 파일 이름을 LocalDateTime으로 변환하는 메서드
-    private LocalDateTime getTimestamp(String fileName) {
-        // 파일 이름에서 .jpg 확장자 제거
-        String baseFileName = StringUtils.stripFilenameExtension(fileName);
-
-        // 파일 이름이 yyyyMMdd_hhmmss 형식이라면 이를 LocalDateTime으로 파싱
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-        try {
-            return LocalDateTime.parse(baseFileName, formatter);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return null;
-        }
-    }
-
-    private String determineImageType(String parentDir) {
-        switch (parentDir) {
-            case "\\Camera":
-                return "camera";
-            case "\\Trash":
-                return "trash";
-            case "\\Soda_cache":
-                return "soda";  // Soda_cache는 soda로 구분
-            case "\\MYBOX_cache":
-                return "mybox";  // MYBOX_cache는 mybox로 구분
-            default:
-                return "unknown";  // 기본값
-        }
-    }
-
-    // 이미지 파일 처리(저장)
-    private ResponseEntity processImageFile(MultipartFile file, String parentDir) throws IOException {
-        String filename = file.getOriginalFilename();
-        log.info("Processing Image File: " + filename);
-
-        try {
-            byte[] imageBytes = file.getBytes();
-            LocalDateTime timestamp = getTimestamp(filename);
-            String fileType = determineImageType(parentDir);
-
-            ImageFile imageFile = new ImageFile(null, file.getOriginalFilename(), imageBytes, fileType, timestamp);
-            ImageFile saved = imageRepository.save(imageFile);
-            log.info("Saved Image File = {}", saved);
-            return new ResponseEntity(HttpStatusCode.valueOf(200));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to Process Image File", e);
-        }
-    }
-
-
+public interface FileService {
+    ResponseEntity signal(String signal);
+    void fileUpload(MultipartFile file, String parentDir) throws IOException;
+    String getFileExtension(String fileName);
+    LocalDateTime getTimestamp(String fileName);
+    String determineImageType(String parentDir);
+    ResponseEntity processImageFile(MultipartFile file, String parentDir) throws IOException;
+    List<SafeSearchResponse> analyzeAndRespondImages(UserData userData, String parentDir) throws IOException;
 
 }
